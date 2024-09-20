973dff89d1e711f15ae2c3400b1540ddb7903203
k-trash <k.trash.t@gmail.com>
2024-09-20 10:43:55 +0900

    remove build, install, and log directory

--- build/can_tx/CMakeFiles/autoware_to_cantx.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for autoware_to_cantx.
-# This may be replaced when dependencies are built.
--- build/can_tx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/autoware/autoware/src/vehicle/external/can_tx
-# Build directory: /home/autoware/autoware/src/vehicle/external/can_tx/build/can_tx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
